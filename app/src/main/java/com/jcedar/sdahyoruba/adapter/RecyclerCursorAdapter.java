e277414d6fdf14fa5810edea9b2f51deba11d58c
Afolayan <afolayanseyi@gmail.com>
2016-06-18 15:39:17 +0100

    after a little fix

--- app/src/main/java/com/jcedar/sdahyoruba/adapter/RecyclerCursorAdapter.java
+++ app/src/main/java/com/jcedar/sdahyoruba/adapter/RecyclerCursorAdapter.java
@@ -6,6 +6,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.TextView;
 
 import com.jcedar.sdahyoruba.R;
@@ -15,38 +16,43 @@ import butterknife.Bind;
 import butterknife.ButterKnife;
 
 
-public class RecyclerCursorAdapter extends RecyclerViewCursorAdapter<RecyclerCursorAdapter.SearchResultViewHolder>
+public class RecyclerCursorAdapter extends RecyclerViewCursorAdapter<RecyclerCursorAdapter.HymnViewHolder>
         implements View.OnClickListener
 {
     private final LayoutInflater layoutInflater;
     private OnItemClickListener onItemClickListener;
+    private AdapterView.OnItemLongClickListener onLongClickListener;
+    public Context context;
 
-    public RecyclerCursorAdapter(final Context context)
-    {
+    public RecyclerCursorAdapter(final Context context) {
         super();
 
         this.layoutInflater = LayoutInflater.from(context);
-        setHasStableIds(true);
+        this.context = context;
     }
 
-    public void setOnItemClickListener(final OnItemClickListener onItemClickListener)
-    {
+    public void setOnItemClickListener(final OnItemClickListener onItemClickListener) {
         this.onItemClickListener = onItemClickListener;
     }
 
+    public void setOnItemLongClickListener(final AdapterView.OnItemLongClickListener onItemClickListener) {
+        this.onLongClickListener = onItemClickListener;
+    }
+
     @Override
-    public SearchResultViewHolder onCreateViewHolder(final ViewGroup parent, final int viewType)
+    public HymnViewHolder onCreateViewHolder(final ViewGroup parent, final int viewType)
     {
         final View view = this.layoutInflater.inflate(R.layout.list_n_item, parent, false);
         view.setOnClickListener(this);
 
-        return new SearchResultViewHolder(view);
+        return new HymnViewHolder(view, context);
     }
 
     @Override
-    public void onBindViewHolder(final SearchResultViewHolder holder, final Cursor cursor)
-    {
+    public void onBindViewHolder(final HymnViewHolder holder, final Cursor cursor) {
         holder.bindData(cursor);
+        String id = cursor.getString(cursor.getColumnIndex(DataContract.Hymns.SONG_ID));
+        holder.setTag(id);
     }
 
      /*
@@ -60,41 +66,58 @@ public class RecyclerCursorAdapter extends RecyclerViewCursorAdapter<RecyclerCur
         {
             final RecyclerView recyclerView = (RecyclerView) view.getParent();
             final int position = recyclerView.getChildLayoutPosition(view);
-            if (position != RecyclerView.NO_POSITION)
-            {
+            if (position != RecyclerView.NO_POSITION) {
                 final Cursor cursor = this.getItem(position);
                 this.onItemClickListener.onItemClicked(cursor);
             }
         }
     }
 
-    public static class SearchResultViewHolder extends RecyclerView.ViewHolder
+    public static class HymnViewHolder extends RecyclerView.ViewHolder
     {
         @Bind(R.id.lblName)
         TextView textViewName;
 
-        @Bind(R.id.lblCourse)
-        TextView textViewCourse;
+        @Bind(R.id.lblEnglish)
+        TextView textViewEnglish;
 
-        @Bind(R.id.lblDoB)
-        TextView textViewDOB;
 
-        public SearchResultViewHolder(final View itemView)
-        {
+
+        Context context;
+
+        View view;
+
+        public HymnViewHolder(final View itemView, Context context) {
             super(itemView);
+            this.view = itemView;
             ButterKnife.bind(this, itemView);
+            this.context = context;
         }
 
-        public void bindData(final Cursor cursor)
-        {
+        public void bindData(final Cursor cursor) {
+            String userId = cursor.getString(
+                    cursor.getColumnIndex(DataContract.Hymns._ID));
+
+            final String num = cursor.getString(cursor.getColumnIndex(DataContract.Hymns.SONG_ID));
             final String name = cursor.getString(cursor.getColumnIndex(DataContract.Hymns.SONG_NAME));
-            this.textViewName.setText(name);
 
-            final String course = cursor.getString(cursor.getColumnIndex(DataContract.Hymns.ENGLISH_VERSION));
-            this.textViewCourse.setText(course);
+            String display = num +" - "+name;
+
+            this.textViewName.setText(display);
+
+
+            final String eng = cursor.getString(cursor.getColumnIndex(DataContract.Hymns.ENGLISH_VERSION));
+            this.textViewEnglish.setText(eng);
 
 
         }
+        public void setTag(String string){
+            this.view.setTag( string);
+        }
+        public String getTag(){
+           return this.view.getTag().toString();
+        }
+
     }
 
     public interface OnItemClickListener
